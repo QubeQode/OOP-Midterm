620cdb9324d2fc579da19413e87d51add5820442
QubeQode <77304768+QubeQode@users.noreply.github.com>
2023-02-14 12:00:36 -0800

    Final submission

--- CloudImporter.ts
+++ CloudImporter.ts
@@ -1,3 +1,5 @@
+import { Album } from "./Album";
+import { Artist } from "./Artist";
 import { IImportable } from "./IImportable";
 import { Playlist } from "./Playlist";
 import { Song } from "./Song";
@@ -11,6 +13,14 @@ export class CloudImporter implements IImportable {
 
     loadPlaylist(name: string): Playlist {
         console.log(`Your playlist at location ${this._url} will be loaded`);
+
+        const song1 = new Song('test1');
+        const song2 = new Song('test2');
+        const song3 = new Song('test3');
+
+        const artist = new Artist('someArtist', album1);
+
+        const album1 = new Album('testAlbum', 'someArtist', 1997, [song1, song2, song3]);
 
         const data: Song[] = [];
         return new Playlist(name, data);
